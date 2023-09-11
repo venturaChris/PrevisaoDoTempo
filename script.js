4d3aa404ebae7f8740e12782fa346dc5af83a4ef
Christopher Ventura <99927387+venturaChris@users.noreply.github.com>
2023-09-11 10:44:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
     document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
     document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
-    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%" + " umidade"
     document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
  }
 
